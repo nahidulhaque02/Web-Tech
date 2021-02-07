d543be15460d7aac838344dad49912bec4ec002e
nahidulhaque <nahidulhaquesoumik@gmail.com>
2021-02-07 16:25:03 +0600

    lab3

---
+++ lab3/problem2.php
@@ -0,0 +1,18 @@
+<?php
+	  $marks=90; 
+	  if($marks >=90) {
+		 echo "A+" ;
+	     }
+	  else if($marks >80 && $marks <90){
+	     echo "A" ;
+		 }
+	  else if($marks >70 && $marks<80) {
+	     echo "B";
+	     }
+	  else if($marks >60 && $marks<70){
+	     echo "C";
+	     }
+	  else{
+		  echo "F";
+	  }
+?>
